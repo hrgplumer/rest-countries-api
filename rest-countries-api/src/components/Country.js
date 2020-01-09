d07172e6f8c8df06b04d0236fdc9113e9eac83e2
John Connor <hrgplumer@gmail.com>
2020-01-09 16:01:26 -0500

    more adjustments to grid layout and country card styling

--- rest-countries-api/src/components/Country.js
+++ rest-countries-api/src/components/Country.js
@@ -5,13 +5,15 @@ const Country = ({ countryObject }) => {
     const [country] = useState(countryObject);
     return (
         <div className="country">
-            <h2>{country.name}</h2>
             <div className="flag-cover" style={{ backgroundImage: `url(${country.flag})` }}></div>
-            <ul className="country-specs">
-                <li><strong>Population:</strong> {country.population}</li>
-                <li><strong>Region:</strong> {country.region}</li>
-                <li><strong>Capital:</strong> {country.capital}</li>
-            </ul>
+            <div className="country-detail">
+                <h2>{country.name}</h2>
+                <ul className="country-specs">
+                    <li><strong>Population:</strong> {country.population.toLocaleString()}</li>
+                    <li><strong>Region:</strong> {country.region}</li>
+                    <li><strong>Capital:</strong> {country.capital}</li>
+                </ul>
+            </div>
         </div>
     );
 }
