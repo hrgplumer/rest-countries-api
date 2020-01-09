37c884181853307483f9750dc722cfe44032e314
John Connor <hrgplumer@gmail.com>
2020-01-09 16:56:04 -0500

    get Country card link working -> CountryDetail

--- rest-countries-api/src/components/Country.js
+++ rest-countries-api/src/components/Country.js
@@ -1,20 +1,23 @@
 import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import '../assets/country.scss';
 
 const Country = ({ countryObject }) => {
     const [country] = useState(countryObject);
     return (
-        <div className="country">
-            <div className="flag-cover" style={{ backgroundImage: `url(${country.flag})` }}></div>
-            <div className="country-detail">
-                <h2>{country.name}</h2>
-                <ul className="country-specs">
-                    <li><strong>Population:</strong> {country.population.toLocaleString()}</li>
-                    <li><strong>Region:</strong> {country.region}</li>
-                    <li><strong>Capital:</strong> {country.capital}</li>
-                </ul>
+        <Link to={`/country/${country.numericCode}`}>
+            <div className="country">
+                <div className="flag-cover" style={{ backgroundImage: `url(${country.flag})` }}></div>
+                <div className="country-detail">
+                    <h2>{country.name}</h2>
+                    <ul className="country-specs">
+                        <li><strong>Population:</strong> {country.population.toLocaleString()}</li>
+                        <li><strong>Region:</strong> {country.region}</li>
+                        <li><strong>Capital:</strong> {country.capital}</li>
+                    </ul>
+                </div>
             </div>
-        </div>
+        </Link>
     );
 }
 
--- rest-countries-api/src/components/CountryDetail.js
+++ rest-countries-api/src/components/CountryDetail.js
@@ -2,10 +2,10 @@ import React from 'react';
 import { useParams } from 'react-router-dom';
 
 const CountryDetail = () => {
-    const { countryCode } = useParams();
+    const { countryId } = useParams();
     return (
         <div>
-            country detail
+            {countryId}
         </div>
     );
 }
